e46c2bdafdb94e33c9ee5ff2fc2b2d2d99e6327f
Dmitrii Suvorov <dsuvorov@gmail.com>
2022-09-08 19:46:34 +0300

    Update Package.swift
    // swift-tools-version:5.6 added to the top of the manifest file

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
-    name: "BinUtils"
+    name: "BinUtils",
+   products: [
+        .library(name: "BinUtils", targets: ["BinUtils"])
+    ],
+    targets: [
+        .target(
+            name: "BinUtils",
+            path: "Sources"
+        )
+    ]
 )
